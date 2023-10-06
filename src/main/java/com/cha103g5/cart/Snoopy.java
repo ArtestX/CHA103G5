9e84c9f032b3691fa6f2863302d10eb604bc382a
Leon Lin <firesnoopy0731@gmail.com>
2023-10-06 15:13:26 +0800

    新增一個 Snoopy class from Leon

--- src/main/java/com/cha103g5/cart/Snoopy.java
+++ src/main/java/com/cha103g5/cart/Snoopy.java
@@ -2,7 +2,7 @@ package com.cha103g5.cart;
 
 public class Snoopy {
 	public static void main(String[] args) {
-		System.out.println("Hello G5 Fighting!!!");
+		System.out.println("Hello G5 Fighting!!");
 	}
 
 }
