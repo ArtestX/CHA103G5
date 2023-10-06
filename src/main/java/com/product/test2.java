1d8031acb820579c19d074034083d21b86d46dc4
“andygit config --global user.email “andy88555858@yahoo.com.tw <andy88555858@yahoo.com.tw>
2023-10-06 15:10:36 +0800

    create

---
+++ src/main/java/com/product/test2.java
@@ -0,0 +1,23 @@
+package com.product;
+
+public class test2 {
+	private String brand;
+	private double price;
+	
+	public test2() {
+		super();
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
