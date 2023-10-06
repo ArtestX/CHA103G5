61b738c3265dd17d4ce3c8a124962c81261414d1
Ryan <r803425@gmail.com>
2023-10-06 15:09:18 +0800

    New: create Pen class

---
+++ src/main/java/com/cha103g5/adopt/Pen.java
@@ -0,0 +1,20 @@
+package com.cha103g5.adopt;
+
+public class Pen {
+	private String brand;
+	private double price;
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+}
