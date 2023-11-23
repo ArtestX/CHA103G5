3d2a2d8ec0ba6e0a07d11a60186ddc3dede63d2a
Zoe <z0975305153@gmail.com>
2023-11-24 01:01:14 +0800

    update for Master merge

--- src/main/java/com/cha103g5/membernotice/controller/WebSocketController.java
+++ src/main/java/com/cha103g5/membernotice/controller/WebSocketController.java
@@ -1,5 +1,9 @@
 package com.cha103g5.membernotice.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -7,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 import com.cha103g5.membernotice.model.WebSocketMessage;
+import com.google.gson.Gson;
+
+import redis.clients.jedis.Jedis;
 
 @Controller
 @EnableWebSocketMessageBroker
@@ -15,18 +22,40 @@ public class WebSocketController {
 	@MessageMapping("/sendToAll")
     @SendTo("/topic/messages")
 	public WebSocketMessage sendToAll(WebSocketMessage message) {
-	    System.out.println("Received message - Title: " + message.getTitle() + ", Content: " + message.getContent() + ", Timestamp: " + message.getTimestamp());
-	    return new WebSocketMessage(message.getTitle(), message.getContent(), message.getTimestamp());
+		try {
+	        System.out.println("Received message - Title: " + message.getTitle() + ", Content: " + message.getContent() + ", Timestamp: " + message.getFormattedTimestamp());
+	        
+	        // 將訊息存進 Redis
+	        Map<String, String> bellText = new HashMap<>();
+	        bellText.put("bellmessage", "Title: " + message.getTitle() + ", Content: " + message.getContent() + ", Timestamp: " + message.getFormattedTimestamp());
+
+	        Jedis jedis = new Jedis("localhost", 6379);
+	        String pingResult = jedis.ping();
+	        System.out.println("Ping result: " + pingResult);
+	        jedis.select(3);
+	        Gson gson = new Gson();
+	        String bellmessage = gson.toJson(bellText);
+
+	        // 使用 Timestamp 作為唯一標識符
+	        String messageId = message.getFormattedTimestamp();
+
+	        // 檢查 Redis 是否成功存儲
+	        if ("OK".equals(jedis.set(messageId, bellmessage))) {
+	        	jedis.set(messageId, bellmessage);
+	            System.out.println("Message stored in Redis successfully");
+	        } else {
+	            System.out.println("Failed to store message in Redis");
+	        }
+
+	        jedis.close();
+
+	        return new WebSocketMessage(message.getTitle(), message.getContent(), message.getFormattedTimestamp());
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return null; // 返回 null 或其他錯誤處理方式
+	    }
+	    
 	}
     
-    @GetMapping("/memberNotice/backendMemberNotice")
-    public String ShowBackendMemberNotice(){
-        return "memberNotice/backendMemberNotice";
-    }
-
-    @GetMapping("/memberNotice/frontMemberNotice")
-    public String ShowFrontMemberNotice(){
-        return "memberNotice/frontMemberNotice";
-    }
 }
 
